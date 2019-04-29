250fc397d690419afd3e494ff6a5e003bb50d5d7
Jean Velonis <jean.velonis@gmail.com>
2019-04-29 01:50:37 +0000

    root path

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,8 +3,18 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-  def new 
+  def new
     @quote = Quote.new
-    
+  end
+
+  def create
+    Quote.create(quote_params)
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
   end
 end
+ 
